cf4274cb12b804b719dde2a9667a20f6b2200e0f
zhan0903 <z.han0903@gmail.com>
2019-10-13 22:10:27 +1100

    debug

--- core/mod_utils.py
+++ core/mod_utils.py
@@ -121,7 +121,25 @@ class SumTree:
 
         return (idx, self.tree[idx], self.data[dataIdx])
 
-class NormalizedActions(gym.ActionWrapper):
+class NormalizedActions:#(gym.ActionWrapper):
+    def __init__(self,env_name):
+        self.env = gym.make(env_name)
+        self.action_low = float(self.env.action_space.low[0])
+        self.action_high = float(self.env.action_space.high[0])
+
+    def step(self, action: object): #Expects a numpy action
+        """Take an action to forward the simulation
+            Parameters:
+                action (ndarray): action to take in the env
+            Returns:
+                next_obs (list): Next state
+                reward (float): Reward for this step
+                done (bool): Simulation done?
+                info (None): Template from OpenAi gym (doesnt have anything)
+        """
+
+        action = self.action_low + action * (self.action_high - self.action_low)
+        return self.env.step(action)
 
     def _action(self, action):
         action = (action + 1) / 2  # [-1, 1] => [0, 1]

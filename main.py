dc8d163e8ac39838abaff9f672a7013f9cc8c0c9
zhan0903 <z.han0903@gmail.com>
2019-10-14 15:07:27 +1100

    debug

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ class Agent:
             if store_transition: self.add_experience(state, action, next_state, reward, done)
             state = next_state
         if store_transition: self.num_games += 1
-        print("reward",reward)
+        print("total_reward",total_reward)
 
         return total_reward
 
